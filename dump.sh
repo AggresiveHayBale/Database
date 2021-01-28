381d002e8bfe54506b65b12562397509abca0b46
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-28 14:49:09 +0100

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-/*
-The script will archive the database with naming convention "mongodump_YYYY_MM__DD"
 
-The script require a connection string as a parameter to access the database
-usr - username 
-pass - password 
-database - database name
-server - cluster server names (should be 3)
-*/
+#The script will archive the database with naming convention "mongodump_YYYY_MM__DD"
+#
+#The script require a connection string as a parameter to access the database
+#usr - username 
+#pass - password 
+#database - database name
+#server - cluster server names (should be 3)
+
 
 usr=$1
 pass=$2
@@ -15,7 +15,7 @@ database=$3
 server=$4
 curr_date=$(date +"%Y_%m_%d")
 
-mongodump --host$server --username $usr --password $pass --authenticationDatabase admin --ssl --archive="mongodump_$curr_date" --db $database
+mongodump --host $server --username $usr --password $pass --authenticationDatabase admin --ssl --archive="mongodump_$curr_date" --db $database
 
 
 
