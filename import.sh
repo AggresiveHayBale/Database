7115fc86ae92d8db7f90add3c5250adfc0f52081
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-28 15:30:30 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-/*
-The script will import data to the database. It will update all fields with new values feed to the the script in JSON file.
+#The script will import data to the database. It will update all fields with new values feed to the the script in JSON file.
+#
+#The script require a connection string as a parameter to access the database
+#usr - username 
+#pass - password 
+#file - JSON file with new data
+#server - cluster server names (should be 3)
 
-The script require a connection string as a parameter to access the database
-usr - username 
-pass - password 
-file - JSON file with new data
-server - cluster server names (should be 3)
-*/
 usr=$1
 pass=$2
 file=$3
