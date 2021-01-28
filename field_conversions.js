1c1a71871c43db9ce2bbe52615871535fda17cd0
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-28 16:19:14 +0100

    Update field_conversions.js

--- field_conversions.js
+++ field_conversions.js
@@ -1,19 +1,3 @@
-#!/bin/sh
-#
-#The script will:
-#1. Add ISO date based on Isolation_Date, as a new field 'Date' used by charts. 
-#It will omit any entry where Isolation_Date is uncertain (0,"",20200000,20210000)
-#2. Remove Date from other uncertain elements by searching strings for 0000 
-#3. Will coerce data type and format to the schema:
-#a)change to lowercase fields : Location, Status, RKI_Valid, RKI_Submit
-#b)change to uppercase field : Sample_Type, Seq_Reason 
-#c)change to integer fields : Isolation_Date, Submitting_Lab, Sequencing_Lab, Ct_Value, Analysing_Date, Ambiguous_Bases,Query_Length,Submitting_Lab
-#d)hange to double fields : ACGT_Nucleotide_Identity, ACGT_Nucleotide_Identity_ignoring_Ns,
-#e)change to strings fields : Sample_ID, Location, Sub_Location, Status, Primer, Seq_Reason, Sample_Type, Mutations, Storage, File_Name, GISAID_ID, Comments
-#
-#The script require a connection string as a parameter to access the database
-#connection - connection string to log into database
-
 db = db.getSiblingDB('SARSCoV2') 
 
 db.routineseq.update({},
