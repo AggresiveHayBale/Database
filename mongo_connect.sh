6b1d3e9354c55a396ab278746b66322a06ee2cfb
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-29 10:09:16 +0100

    Update mongo_connect.sh

--- mongo_connect.sh
+++ mongo_connect.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-/* Simple script to automate connecting to the database. 
-It require only connection string
-*/
+# Simple script to automate connecting to the database. 
+#It require only connection string
+
+connection=$1
 
 if [ -z "$1" ]
   then
     echo "No connection string supplied";
     exit 1
 fi
-connection=$1
 
 mongo $connection
 use SARSCoV2
