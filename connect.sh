437e86e51f5ed46509685d1b34252d818b1d6304
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-29 10:52:30 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -4,6 +4,13 @@
 
 connection=$1
 
+#Sanity checks, check if MongoDB Database Tools were installed 
+if ! command -v mongo &> /dev/null
+  then
+    echo "MongoDB Shell could not be found, please install the tool"
+    exit 
+fi
+
 if [ -z "$1" ]
   then
     echo "No connection string supplied";
