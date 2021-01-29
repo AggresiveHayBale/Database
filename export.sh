531402669ae1171d008f45265d9d5b748d2b939f
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-29 10:16:44 +0100

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-#The script will export database entries to the database.
+#The script will export database entries to the file.
+#usr-username, pass-password, file-outputfile name, server-cluster server names, 
+#list_fields - fields that would be exported(e.g. Sample_ID), filter - the query what files should be exported (e.g.'{"Sample_ID": "XX0000"}')
 
-usr=
-pass=
-file=.json
-server=
-list_fields=Sample_ID
-filter='{"Sample_ID": "me40609"}'
+usr=$1
+pass=$2
+file=$3
+server=$4
+list_fields=$5
+filter=$6
 
-mongoexport --host $server --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --fields $list_fields --query '{"Sample_ID": "me40609"}' --type json --out test.json --pretty
+mongoexport --host $server --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --fields $list_fields --query $filter --type json --out $file --pretty
