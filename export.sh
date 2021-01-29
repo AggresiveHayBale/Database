e395fbd4c4c6636a439944e98e691100938c0db9
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-29 12:53:37 +0100

    Update export.sh

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@
 
 #The script will export database entries to the file.
 #usr-username, pass-password, file-outputfile name, server-cluster server names, 
-#list_fields - fields that would be exported(e.g. Sample_ID), filter - the query what files should be exported (e.g.'{"Sample_ID": "XX0000"}')
+#list_fields - fields that would be exported(e.g. Sample_ID) if set to full it will export all of the possible fields, filter - the query what files should be exported (e.g.'{"Sample_ID": "XX0000"}')
 
 usr=$1
 pass=$2
@@ -12,10 +12,43 @@ list_fields=$5
 filter=$6
 
 #Sanity checks, check if MongoDB Database Tools were installed 
-if ! command -v mongoimport &> /dev/null
+if ! command -v mongoimport 
   then
     echo "MongoDB Database Tools could not be found, please install the tool"
     exit 
 fi
+if [ -z "$1" ]
+  then
+    echo "No user specified";
+    exit 
+fi
+if [ -z "$2" ]
+  then
+    echo "No password specified";
+    exit 
+fi
+if [ -z "$3" ]
+  then
+    echo "No cluster server names specified";
+    exit 
+fi
+if [ -z "$4" ]
+  then
+    echo "No output file specified";
+    exit 
+fi
 
-mongoexport --host $server --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --fields $list_fields --query $filter --type json --out $file --pretty
+if [ $5="full" ]
+	then
+	mongoexport --host "$server" --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --query "$filter" --type json --out $file --pretty
+elif [ -z "$6" ]
+	then 
+	mongoexport --host "$server" --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --fields $list_fields --type json --out $file --pretty
+else 
+	mongoexport --host "$server" --username $usr --password $pass --authenticationDatabase admin --ssl --db SARSCoV2 --collection routineseq --fields $list_fields --query "$filter" --type json --out $file --pretty
+fi
+
+
+
+
+
