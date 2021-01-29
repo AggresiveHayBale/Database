73411bd4efe9742ef922feec63aab0eb1a7b036a
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-01-29 10:23:54 +0100

    Update field_mod_exec.sh

--- field_mod_exec.sh
+++ field_mod_exec.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-#Script to execute scripts for geo.js and fieldconv .
+#Script to execute scripts for geo.js and field_conversions.js.
+
+#Parameter:
+#connection - connection string to log into database
 
 #geo.js:
 #The script will add new array with geographical coordinates based on location.
 #New coordinates need to be put manualy from the website http://geojson.io.
 #
 #The script require a connection string as a parameter to access the database
-#connection - connection string to log into database*/
-#
+
+
 # Current locations in the database:
-#    "eisenach",
+#   "eisenach",
 #  	"greiz",
 #  	"heiligenstadt",
 #  	"hildburghausen",
@@ -22,8 +25,7 @@
 # 	"suhl",
 #  	"wartburgkreis",
 #  	"weimar"
-#
-#
+
 #Helper commands that need to be executed manualy to detect new fields 
 #
 #Show unique locations
@@ -39,14 +41,9 @@
 #3. Will coerce data type and format to the schema:
 #a)change to lowercase fields : Location, Status, RKI_Valid, RKI_Submit
 #b)change to uppercase field : Sample_Type, Seq_Reason 
-#c)change to integer fields : Isolation_Date, Submitting_Lab, Sequencing_Lab, Ct_Value, Analysing_Date, Ambiguous_Bases,Query_Length,Submitting_Lab
+#c)change to integer fields : Isolation_Date, Submitting_Lab, Sequencing_Lab, Ct_Value, Analysing_Date, Ambiguous_Bases, Query_Length, Submitting_Lab
 #d)hange to double fields : ACGT_Nucleotide_Identity, ACGT_Nucleotide_Identity_ignoring_Ns,
 #e)change to strings fields : Sample_ID, Location, Sub_Location, Status, Primer, Seq_Reason, Sample_Type, Mutations, Storage, File_Name, GISAID_ID, Comments
-#
-#The script require a connection string as a parameter to access the database
-#connection - connection string to log into database
-
-
 
 
 
@@ -58,5 +55,5 @@ if [ -z "$1" ]
 fi
 connection=$1
 
-mongo $connection <geo.js
 mongo $connection <field_conversions.js
+mongo $connection <geo.js
