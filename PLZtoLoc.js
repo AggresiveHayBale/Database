803b4493cd602eb868f4befb70af54a071ffb5c9
Mateusz Jundzill <72441834+AggresiveHayBale@users.noreply.github.com>
2021-02-17 12:52:59 +0100

    Create PLZtoLoc.js

---
+++ PLZtoLoc.js
@@ -0,0 +1,19 @@
+db = db.getSiblingDB('SARSCoV2')
+db.routineseq.aggregate(
+[{$match: {
+  PLZ: {
+    $exists: true
+  },
+  Location: {"$in":[null,""]}
+}}, {$lookup: {
+  from: 'GeoData',
+  localField: 'PLZ',
+  foreignField: 'PLZ',
+  as: 'TempLoc'
+}}, {$set: {
+  Location: '$TempLoc.city'
+}}, {$unwind: {
+  path: '$Location'
+}}, {$project: {
+  TempLoc: 0
+}}])
